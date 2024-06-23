d7d75b1d6b0807b1e4ba7b1cccf2630d50ae414e
ryanms1 <ryan_shippley@yahoo.com>
2024-06-22 18:15:05 -0700

    finished admin products page

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -3,6 +3,7 @@
 import db from "@/db/db"
 import { z } from "zod"
 import fs from "fs/promises"
+import { notFound, redirect } from "next/navigation"
 
 const fileSchema = z.instanceof(File, { message: "Required" })
 const imageSchema = fileSchema.refine(
@@ -16,7 +17,7 @@ const addSchema = z.object({
     file: fileSchema.refine(file => file.size > 0, "Required"),
     image: imageSchema.refine(file => file.size > 0, "Required")
 })
-export async function addProduct(formData: FormData) {
+export async function addProduct(prevState: unknown, formData: FormData) {
     const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
     if (result.success === false) {
         return result.error.formErrors.fieldErrors
@@ -34,6 +35,7 @@ export async function addProduct(formData: FormData) {
 
     await db.product.create({
         data: {
+            isAvailableForPurchase: false,
             name: data.name,
             description: data.description,
             priceInCents: data.priceInCents,
@@ -41,4 +43,67 @@ export async function addProduct(formData: FormData) {
             filePath
         }
     })
+
+    redirect("/admin/products")
+}
+
+const editSchema = addSchema.extend({
+    file: fileSchema.optional(),
+    image: imageSchema.optional()
+})
+
+export async function updateProduct(id: string, prevState: unknown, formData: FormData) {
+    const result = editSchema.safeParse(Object.fromEntries(formData.entries()))
+    if (result.success === false) {
+        return result.error.formErrors.fieldErrors
+    }
+    const data = result.data
+    const product = await db.product.findUnique({ where: { id } })
+
+    if (product == null) return notFound()
+
+    let filePath = product.filePath
+    if (data.file != null && data.file.size > 0) {
+        await fs.unlink(product.filePath)
+        filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+        await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+    }
+
+    let imagePath = product.imagePath
+    if (data.image != null && data.image.size > 0) {
+        await fs.unlink(`public${product.imagePath}`)
+        imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+        await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+    }
+
+    await db.product.update({
+        where: { id },
+        data: {
+            name: data.name,
+            description: data.description,
+            priceInCents: data.priceInCents,
+            imagePath,
+            filePath
+        }
+    })
+
+    redirect("/admin/products")
+}
+
+export async function toggleProductAvailability(id: string,
+    isAvailableForPurchase: boolean) {
+    await db.product.update({
+        where: { id }, data: {
+            isAvailableForPurchase
+        }
+    })
+}
+
+export async function deleteProduct(id: string) {
+    const product = await db.product.delete({ where: { id } })
+
+    if (product == null) return notFound()
+
+    await fs.unlink(product.filePath)
+    await fs.unlink(`public${product.imagePath}`)
 }
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
 import Link from "next/link";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import db from "@/db/db";
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ActiveToggleDropdownItem, DeleteDropdownItem } from "./_components/ProductsActions";
+import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
 
 export default function AdminProductsPage() {
     return (
@@ -17,7 +23,19 @@ export default function AdminProductsPage() {
     )
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+    const products = await db.product.findMany({
+        select: {
+            id: true,
+            name: true,
+            priceInCents: true,
+            isAvailableForPurchase: true,
+            _count: { select: { orders: true } }
+        }
+    })
+
+    if (products.length === 0) return <p>No products found</p>
+
     return (
         <Table>
             <TableHeader>
@@ -33,6 +51,56 @@ function ProductsTable() {
                     </TableHead>
                 </TableRow>
             </TableHeader>
+            <TableBody>
+                {products.map(product => (
+                    <TableRow key={product.id}>
+                        <TableCell>
+                            {product.isAvailableForPurchase ? (
+                                <>
+                                    <span className="sr-only">Available</span>
+                                    <CheckCircle2 />
+                                </>
+                            ) : (
+                                <>
+                                    <span className="sr-only">Unavailable</span>
+                                    <XCircle className="stroke-destructive" />
+                                </>
+                            )}
+                        </TableCell>
+                        <TableCell>{product.name}</TableCell>
+                        <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+                        <TableCell>{formatNumber(product._count.orders)}</TableCell>
+                        <TableCell>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <MoreVertical />
+                                    <span className="sr-only">Actions</span>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DropdownMenuItem asChild>
+                                        <a download href={`/admin/products/${product.id}/download`}>
+                                            Download
+                                        </a>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem asChild>
+                                        <Link href={`/admin/products/${product.id}/edit`}>
+                                            Edit
+                                        </Link>
+                                    </DropdownMenuItem>
+                                    <ActiveToggleDropdownItem id={product.id}
+                                        isAvailableForPurchase={product.isAvailableForPurchase}
+                                    />
+                                    <DropdownMenuSeparator />
+                                    <DeleteDropdownItem
+                                        id={product.id}
+                                        disabled={product._count.orders > 0}
+                                    />
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
         </Table>
     )
 }
